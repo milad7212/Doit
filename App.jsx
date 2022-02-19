ce95fa15bc3d3315cc85dd3b832377a8d50cbe42
miillaadddddd <milad7212@gmail.com>
2022-02-19 10:41:07 +0330

    Feat : Add ListingDetailsScreen component

--- App.jsx
+++ App.jsx
@@ -24,17 +24,10 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View style={{ backgroundColor: "#f8f4f", padding: 20, paddingTop: 100 }}>
-      <Card
-        title="white jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/person.jpeg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
 
 const styles = StyleSheet.create({

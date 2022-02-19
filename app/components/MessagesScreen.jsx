4e9341afedfa002a6466869504bcf3df19796d80
miillaadddddd <milad7212@gmail.com>
2022-02-19 12:14:05 +0330

    Feat : Handling Selections

--- app/components/MessagesScreen.jsx
+++ app/components/MessagesScreen.jsx
@@ -28,6 +28,7 @@ function MessagesScreen(props) {
             title={item.title}
             subTitle={item.description}
             image={item.image}
+            onPress={() => console.log(item)}
           />
         )}
       />
